5f27015b0df178919c9831d63cdb399fa58c8213
rrichley <robrichley@hotmail.co.uk>
2025-05-20 13:27:49 +0100

    Init and format data disk before AD DS promotion

---
+++ install-ad.ps1
@@ -0,0 +1,26 @@
+# Initialise the new data disk (LUN 0) as F:
+$disk = Get-Disk | Where-Object PartitionStyle -Eq 'RAW' | Sort-Object Number | Select-Object -First 1
+Initialize-Disk -Number $disk.Number -PartitionStyle MBR
+New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ADData" -Confirm:$false
+$driveLetter = (Get-Partition -DiskNumber $disk.Number | Get-Volume).DriveLetter
+$ntdsPath = "$driveLetter`:\Windows\NTDS"
+$sysvolPath = "$driveLetter`:\Windows\SYSVOL"
+
+# Install AD DS and Management Tools
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Promote to Domain Controller
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath $ntdsPath `
+    -DomainMode "WinThreshold" `
+    -DomainName "rr.com" `
+    -DomainNetbiosName "RR" `
+    -ForestMode "WinThreshold" `
+    -InstallDns:$true `
+    -LogPath $ntdsPath `
+    -SysvolPath $sysvolPath `
+    -NoRebootOnCompletion:$false `
+    -Force:$true
