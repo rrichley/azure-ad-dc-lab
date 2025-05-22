45c02502c6a77943bebe2e9be1968dd3b34f3978
rrichley <94038906+rrichley@users.noreply.github.com>
2025-05-22 14:09:24 +0100

    Update install-ad.ps1

--- install-ad.ps1
+++ install-ad.ps1
@@ -4,30 +4,8 @@ Start-Transcript -Path "C:\ad-setup.log" -Force
 $disk = Get-Disk | Where-Object PartitionStyle -Eq 'RAW' | Sort-Object Number | Select-Object -First 1
 Initialize-Disk -Number $disk.Number -PartitionStyle MBR
 New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ADData" -Confirm:$false
-$driveLetter = (Get-Partition -DiskNumber $disk.Number | Get-Volume).DriveLetter
-$ntdsPath = "$driveLetter`:\Windows\NTDS"
-$sysvolPath = "$driveLetter`:\Windows\SYSVOL"
 
 # Install AD DS and Management Tools
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
-# Promote to Domain Controller
-Import-Module ADDSDeployment
-
-Install-ADDSForest `
-    -CreateDnsDelegation:$false `
-    -DatabasePath $ntdsPath `
-    -DomainMode "WinThreshold" `
-    -DomainName "rr.com" `
-    -DomainNetbiosName "RR" `
-    -ForestMode "WinThreshold" `
-    -InstallDns:$true `
-    -LogPath $ntdsPath `
-    -SysvolPath $sysvolPath `
-    -NoRebootOnCompletion:$true `
-    -Force:$true
-
 Stop-Transcript
-
-# Commented out to allow Custom Script Extension to return success to Terraform
-# Restart-Computer -Force
